194161e3cc1cab571b9caefcb9cc723083de28c9
billmark0093 <billmark0093@gmail.com>
2024-10-11 15:42:01 -0400

    Middleware app/tx cleared, and vars are declared.

--- backend/index.js
+++ backend/index.js
@@ -7,3 +7,14 @@ const pinataSDK = require('@pinata/sdk');
 const fs = require('fs');
 const glog = require('glog-parser');
 
+const app = express();
+const port = 3100;
+const { PINATA_JWT_KEY } = process.env;
+const pinata = new pinataSDK({ pinataJWTKey: PINATA_JWT_KEY });
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());glog();
+const upload = multer({ dest: 'uploads/' });
+
+
