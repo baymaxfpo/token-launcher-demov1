0c043f88175762e1a8ebc1db3fafca5614fd4f92
billmark0093 <billmark0093@gmail.com>
2024-10-11 15:41:34 -0400

    Refactoring branch is needs to be merge

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,8 @@
 export const SOL_PRICE_API =
   "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd";
 
+<<<<<<< HEAD
 export const SOLANA_RPC = process.env.NEXT_PUBLIC_SOLANA_RPC ?? "";
+=======
+export const SOLANA_RPC = process.env.NEXT_PUBLIC_SOLANA_RPC ?? "https://api.devnet.solana.com";
+>>>>>>> 017d6da (initial commit)
