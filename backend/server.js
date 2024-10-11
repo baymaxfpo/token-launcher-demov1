17196d95e5f246ba2002e29cb377fe31c06bd8ff
billmark0093 <billmark0093@gmail.com>
2024-10-11 15:42:02 -0400

    server.js But/tag1 detected.

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ const fs = require('fs');
 const app = express();
 const PORT = 3000;
 
+
+const upload = multer({ storage: storage });
+
+app.use(express.static('public'));
+app.use('/uploads', express.static('uploads'));
+
 // Configure multer for file uploads
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -16,16 +22,6 @@ const storage = multer.diskStorage({
         cb(null, uniqueSuffix + path.extname(file.originalname));
     }
 });
-const upload = multer({ storage: storage });
-
-app.use(express.static('public'));
-app.use('/uploads', express.static('uploads'));
-
-// Endpoint to handle image uploads
-app.post('/upload', upload.single('image'), (req, res) => {
-    res.json({ imageUrl: `/uploads/${req.file.filename}` });
-});
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
