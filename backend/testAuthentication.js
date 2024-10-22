eaa6ec760c21f4744a1d8b14468a76745814b3e3
billmark0093 <billmark0093@gmail.com>
2024-10-22 09:52:55 -0400

    tested Vars.

--- backend/testAuthentication.js
+++ backend/testAuthentication.js
@@ -3,3 +3,18 @@ const { PINATA_JWT_KEY} = process.env;
 const pinataSDK = require('@pinata/sdk');
 const pinata = new pinataSDK({ pinataJWTKey: PINATA_JWT_KEY});
 
+const fs = require('fs');
+const options = {
+    pinataMetadata: {
+        name: "Hello-World-Wallpaper.jpg",
+        keyvalues: {
+            key_1: 'value_1'
+        }
+    },
+};
+pinata.pinFileToIPFS(readableStreamForFile, options).then((result) => {
+    console.log(result);
+}).catch((err) => {
+    //handle error here
+    console.log(err);
+});
