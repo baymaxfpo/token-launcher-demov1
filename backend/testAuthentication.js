5f9af8a02a987055a0f811a5d7bcde0bba58a6fa
billmark0093 <billmark0093@gmail.com>
2024-10-24 17:46:56 -0400

    Read strem placeholder added. testing..

--- backend/testAuthentication.js
+++ backend/testAuthentication.js
@@ -4,6 +4,7 @@ const pinataSDK = require('@pinata/sdk');
 const pinata = new pinataSDK({ pinataJWTKey: PINATA_JWT_KEY});
 
 const fs = require('fs');
+const readableStreamForFile = fs.createReadStream('./images/Hello-World-Wallpaper.jpg');
 const options = {
     pinataMetadata: {
         name: "Hello-World-Wallpaper.jpg",
@@ -11,6 +12,9 @@ const options = {
             key_1: 'value_1'
         }
     },
+    pinataOptions: {
+        cidVersion: "1"
+    }
 };
 // We pass in the readable stream for the file, ******and****** the options object.
 pinata.pinFileToIPFS(readableStreamForFile, options).then((result) => {
